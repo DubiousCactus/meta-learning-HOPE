45a913af51025dff6296f9bd5c12b61cd4e04df3
Theo Morales <theo.morales.fr@gmail.com>
2021-08-06 18:48:32 +0100

    Revert

--- algorithm/wrappers.py
+++ algorithm/wrappers.py
@@ -285,7 +285,7 @@ class MAML_GraphUNetTrainer(MAMLTrainer):
         gpu_numbers: List = [0],
     ):
         super().__init__(
-            GraphUNetBatchNorm(),
+            "graphunet",
             dataset,
             checkpoint_path,
             k_shots,
